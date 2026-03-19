4be2e100c089a695f2083cf0edc8ad6463e0b3a9
testpython1234589-dev <testpython1234589@gmail.com>
2026-03-19 13:08:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -220,6 +220,8 @@ VORSTEUERBERECHTIGUNG: JA -> "" (leer), NEIN -> "nicht".
 VERSICHERUNG: Wenn vorhanden, immer die Abkürzung/Kurzbezeichnung statt des vollständigen Namens verwenden, sonst vollen Namen.
 Zum Fahrzeugtyp einfach nur Name und Modellbezeichnung 
 -BEACHTE: WIEDERBESCHAFFUNGSWERTAUFWAND = (Wiederbeschaffungswert - Restwert)
+-Falls Keine Abmelde/-Ummeldekosten vorhanden sind so ist : 0,00 € (zu kennzeichnen)
+-Falls Zusätzliche kosten Vorhanden sind so nehmen leerstelle mit bezeichnung: Name, und Betrag:geld betrag
 
 Mandanten Name mit Herr oder Frau siehe Gutachten (meist 1. Seite)
 
