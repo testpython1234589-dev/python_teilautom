d1150ca3b24742939960abfa3359e40f28ec6a39
testpython1234589-dev <testpython1234589@gmail.com>
2026-03-23 11:56:50 +0100

    Update word_backend.py

--- word_backend.py
+++ word_backend.py
@@ -70,7 +70,7 @@ def render_word(tpl_name: str, context: Dict[str, Any], out_prefix: str) -> Path
     tpl.render(context)
 
     nachname = safe_filename(str(context.get("MANDANT_NACHNAME", "Unbekannt")))
-    timestamp = datetime.now().strftime("%d-%m-%Y)
+    timestamp = datetime.now().strftime("%d-%m-%Y")
     out_name = f"{"01-AS_an_VR"}_{timestamp}.docx"
     out_path = OUTPUT_DIR / out_name
 
